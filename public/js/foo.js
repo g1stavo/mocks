e4b25857de65895673be98f6fe7f54d44fd47299
Gustavo <gustavo.salvador@chaordicsystems.com>
2018-04-13 17:27:56 -0300

    Generalization (#2)
    * I can't remember what I did
    
    * Fix routing problem
    
    * Remove dropdown
    
    * On our way
    
    * Fix readCookie
    
    * Change checkbox order

--- public/js/foo.js
+++ public/js/foo.js
@@ -7,20 +7,38 @@ const homeMeta = {
             "referrer":"https://www.google.com.br/"
         }
     },
-    "dataGroup":"netshoes-freedom",
     "host":"www.netshoes.com.ar"
 };
 
 var chaordic_meta = homeMeta;
 
-var setApikey = (apikey) => {
+var isFreedom = () => {
+    const loader = document.getElementById('loader');
+    if (readCookie("freedom") == "true") {
+        loader.setAttribute('data-group', 'netshoes-freedom');
+    } else {
+        let attr = $(loader).attr('data-group');
+        if (typeof attr !== typeof undefined && attr != false) {
+            loader.removeAttribute('data-group');
+        }
+    }
+};
+
+var setApikey = () => {   
+    const apikey = document.getElementById('apikey').value;
+    const loader = document.getElementById('loader');
+    if (document.getElementById('checkbox').checked) {
+        chaordic.writeCookie("freedom", "true");
+    } else {
+        chaordic.writeCookie("freedom", "false");
+    }
     home();
     chaordic.setActiveFoo(apikey);
     location.reload(true);
 };
 
 var product = () => {
-    let id = document.getElementById("pid").value;
+    let id = document.getElementById('pid').value;
     chaordic_meta = {
         "product":{
             "id": id
@@ -35,4 +53,19 @@ var product = () => {
 var home = () => {
     chaordic_meta = homeMeta;
     chaordic.initialize();
+  
+var readCookie = (cookieName) => {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 };
